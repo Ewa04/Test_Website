f442ac1cdf06e678748bd53c0978e297ee18209a
Ewa04 <160962694+Ewa04@users.noreply.github.com>
2025-09-24 14:34:15 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,20 +3,20 @@ self.addEventListener("install", e => {
     e.waitUntil(
         caches.open("static").then(cache => {
             return cache.addAll([
-                "./",
-                ".manifest.json",
+                "./index.html",
+                "./manifest.json"
                 "./icons/logo192.png"
             ]);
         })
     );
     console.log("Resources in cache. Done");
-})
+});
 
 self.addEventListener("fetch", e => {
-    console.log('intercepting fecth request for:', e.request.url);
+    console.log("Intercepting fetch request for:", e.request.url);
     e.respondWith(
         caches.match(e.request).then(response => {
             return response || fetch(e.request);
         })
-    )
+    );
 });
